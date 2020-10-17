e5aaaf35a0a32a3d81aaa07693eb5eddb9ccbb06
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-10-17 12:04:45 +1000

    add tests

--- examples/App.vue
+++ examples/App.vue
@@ -20,52 +20,14 @@
 <script>
 import { ref } from 'vue'
 import { DateTime } from 'luxon'
+import { 
+  serialize, 
+  deserialize,
+  serializeMoment,
+  deserializeMoment
+} from './date-time-serializers.js'
 import moment from 'moment'
 import dateTime from './date-time.vue'
-
-export function serializeMoment(value) {
-  console.log(value)
-  const toString = `${value.year}-${value.month.padStart(2, '0')}-${value.day.padStart(2, '0')}`
-  const toObject = moment(toString, 'YYYY-MM-DD', true)
-  if (toObject.isValid()) {
-    return toObject
-  }
-  return value
-}
-
-export function deserializeMoment(value) {
-  if (!moment.isMoment(value)) {
-    return value
-  }
-
-  return {
-    year: value.year().toString(),
-    month: (value.month() + 1).toString(),
-    day: value.date().toString()
-  }
-}
-
-
-export function deserialize(value) {
-  return {
-    year: value.get('year'),
-    month: value.get('month'),
-    day: value.get('day')
-  }
-}
-
-function serialize(value) {
-  try {
-    const obj = DateTime.fromObject(value)
-    if (obj.invalid) {
-      return 
-    }
-  } catch {
-    return 
-  }
-
-  return DateTime.fromObject(value)
-}
 
 export default {
   components: { dateTime },
